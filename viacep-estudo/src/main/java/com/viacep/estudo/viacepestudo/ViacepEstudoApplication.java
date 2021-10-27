7d895e9d7386e3dcdfbe6bd98334fc9a828b348d
macalsr <mmariasribeiro@gmail.com>
2021-10-27 18:15:16 -0300

    corrigindo docs

---
+++ viacep-estudo/src/main/java/com/viacep/estudo/viacepestudo/ViacepEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.viacep.estudo.viacepestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViacepEstudoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViacepEstudoApplication.class, args);
+    }
+
+}
---
+++ viacep-estudo/src/test/java/com/viacep/estudo/viacepestudo/ViacepEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viacep.estudo.viacepestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViacepEstudoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
